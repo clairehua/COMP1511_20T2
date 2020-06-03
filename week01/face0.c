1c9311281ee82a81a11b252b372eabef685fc7ee
Claire <claire.hua@unsw.edu.au>
2020-06-03 14:59:17 +1000

    week 1 content added

---
+++ week01/face0.c
@@ -0,0 +1,16 @@
+// Prints out a face 
+// COMP1511 W11A, June 3 2020
+
+
+#include <stdio.h>
+
+int main (void) {
+
+    printf("~  ~\n");
+    printf("0  0\n");
+    printf("  o\n");
+    printf("  -\n");
+    
+    return 0;
+}
+
