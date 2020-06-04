6b70fb69c07b270f3ecbfb880387f97bd4873cee
Claire <claire.hua@unsw.edu.au>
2020-06-04 12:46:40 +1000

    added week01 content

---
+++ H11A/week01/face0.c
@@ -0,0 +1,12 @@
+// prints out a face
+// H11A, June 4
+
+#include <stdio.h>
+
+int main (void) {
+    printf("~  ~\n");
+    printf("0  0\n");
+    printf("  o\n");
+    printf("  -\n");
+    return 0;
+}
---
+++ H11A/week01/face1.c
@@ -0,0 +1,22 @@
+// prints out a face (special)
+// H11A, June 4
+
+#include <stdio.h>
+
+int main (void) {
+
+
+    // using one printf statement
+
+    printf("~  ~\n0  0\n  o\n \\_/\n");
+    /* printf("");
+    printf("");
+    printf(""); */
+    
+    // using four
+    printf("~  ~\n");
+    printf("0  0\n");
+    printf("  o\n");
+    printf(" \\_/\n");
+    return 0;
+}
