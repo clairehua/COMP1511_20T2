c54946e2913f7d92934399447d8737d6dff10449
Claire <claire.hua@unsw.edu.au>
2020-07-15 12:37:29 +1000

    w11a week07 content added

---
+++ W11A/week07/hi.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main(void) {
+    char str[10];
+    
+    str[0] = 'H';
+    str[1] = 'i';
+    str[2] = '\0';
+    
+    printf("%s\n", str);
+    
+    return 0;
+}
---
+++ W11A/week07/size.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+int main(void) {
+
+    printf("The size of an int is %lu\n", sizeof(int));
+    printf("The size of a char is %lu\n", sizeof(char));
+    return 0;
+}
