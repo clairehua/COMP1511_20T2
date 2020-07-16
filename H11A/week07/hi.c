00ffe4c134aa6d9ad6931fe247438fe564df074e
Claire <claire.hua@unsw.edu.au>
2020-07-16 12:34:48 +1000

    week07 added, files cleaned up

---
+++ H11A/week07/hi.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main() {
+    char str[10];
+    
+    str[0] = 'H';
+    str[1] = 'i';
+    str[2] = 'H';
+    str[3] = 'i';
+    
+    printf("%s\n", str);
+    return 0;
+}
